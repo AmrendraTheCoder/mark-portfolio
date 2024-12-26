0257fe1ae90a5ff13baa4b7c2f8b693aea2d9b32
Amrendra <leostereo1108@gmail.com>
2024-12-26 19:39:00 +0530

    starting...
    Starting with the Header.jsx.

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/HEader";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
+});
+
+export const metadata = {
+  title: "Mark-Portfolio",
+  description: "Developed by Amrendra",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={jetbrainsMono.variable}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
